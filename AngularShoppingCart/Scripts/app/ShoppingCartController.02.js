dfacf9d5d996745b2bfd7ba7529c5e3b96c53629
S Ravi Kiran <yuvakiran2009@gmail.com>
2013-03-14 00:42:13 +0530

    Added some new files and renamed some existing files

---
+++ AngularShoppingCart/Scripts/app/ShoppingCartController.02.js
@@ -0,0 +1,61 @@
+﻿function ShoppingCartCtrl($scope, shoppingData) {
+    $scope.items = [];
+
+    $scope.item = {};
+
+    $scope.sortExpression = "Name";
+
+    function refreshItems() {
+        shoppingData.getAllItems().then(function (data) {
+            $scope.items = data;
+        },
+                function (errorMessage) {
+                    assignError(errorMessage);
+                });
+    };
+
+    $scope.addItem = function (item) {
+        shoppingData.addAnItem(item).then(function () {
+            refreshItems();
+        },
+                function (errorMessage) {
+                    assignError(errorMessage);
+                });
+
+        $scope.item = {};
+        $scope.itemForm.$setPristine();
+    };
+
+    $scope.removeItem = function (id) {
+        shoppingData.removeItem(id).then(function () {
+            refreshItems();
+        },
+                function (errorMessage) {
+                    assignError(errorMessage);
+                });
+    };
+
+    function assignError(errorMessage) {
+        $scope.error = errorMessage;
+    };
+
+    $scope.mySortFunction = function (item) {
+        if (isNaN(item[$scope.sortExpression]))
+            return item[$scope.sortExpression];
+        return parseInt(item[$scope.sortExpression]);
+    };
+
+    $scope.totalPrice = function () {
+        var total = 0;
+        for (count = 0; count < $scope.items.length; count++) {
+            total += $scope.items[count].Price * $scope.items[count].Quantity;
+        }
+        return total;
+    };
+
+    $scope.name = /^[a-zA-Z ]*$/;
+
+    $scope.integerval = /^\d*$/;
+
+    refreshItems();
+}
---
+++ AngularShoppingCart/Scripts/app/ShoppingModule.02.js
@@ -0,0 +1,66 @@
+﻿angular.module('shopping', []).filter('rupee', function () {
+			return function (item) {
+				return "Rs. " + item;
+			}
+		})
+		.directive('validPrice', function () {
+			return {
+				require: "ngModel",
+				link: function (scope, elm, attrs, ctrl) {
+
+					var regex = /^\d{2,4}(\.\d{1,2})?$/;
+					ctrl.$parsers.unshift(function (viewValue) {
+						var floatValue = parseFloat(viewValue);
+						if (floatValue >= 50 && floatValue <= 5000 && regex.test(viewValue)) {
+							ctrl.$setValidity('validPrice', true);
+							
+						}
+						else {
+							ctrl.$setValidity('validPrice', false);
+						}
+						return viewValue;
+					});
+				}
+			};
+		})
+		.factory('shoppingData',function($http, $q){
+			return{
+				apiPath:'/api/shoppingCart/',
+				getAllItems: function(){
+					//Creating a deferred object
+					var deferred = $q.defer();
+
+					//Calling Web API to fetch shopping cart items
+					$http.get(this.apiPath).success(function(data){
+		    
+            		//Passing data to deferred's resolve function on successful completion
+					deferred.resolve(data);
+				}).error(function(){
+			    
+                	//Sending a friendly error message incase of failure
+					deferred.reject("An error occured while fetching items");
+				});
+
+				//Returning the promise object
+				return deferred.promise;
+			},
+			addAnItem: function(item){
+				var deferred = $q.defer();
+				$http.post(this.apiPath,item).success(function(){
+					deferred.resolve();
+				}).error(function(){
+					deferred.reject("An error occured while adding the new item");
+				});
+				return deferred.promise;
+			},
+			removeItem: function(id){
+				var deferred = $q.defer();
+				$http.delete(this.apiPath+id).success(function(){
+			    	deferred.resolve();
+				}).error(function(){
+					deferred.reject("An error occured while deleting item");
+				});
+				return deferred.promise;
+			}
+		}
+	});
