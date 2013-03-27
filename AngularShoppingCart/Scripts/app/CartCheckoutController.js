ce520788e5b6d0f52768bf5171c655945d7f6539
S Ravi Kiran <yuvakiran2009@gmail.com>
2013-03-27 23:12:41 +0530

    Added a new view, 2 templates and 3 scripts to support multiple views

---
+++ AngularShoppingCart/Scripts/app/CartCheckoutController.js
@@ -0,0 +1,22 @@
+﻿function CartCheckoutCtrl($scope,shared) {
+    $scope.userDetails = {};
+
+    $scope.detailsSubmitted = false;
+
+    $scope.message = "";
+
+    $scope.checkoutCart = function () {
+        $scope.message = "Thank you " + $scope.userDetails.Name + " for using angular shopping cart!\n";
+        $scope.message = $scope.message + "We will call on your number " + $scope.userDetails.PhoneNumber + " soon to get your address details.";
+        $scope.userDetails = {};
+        $scope.detailsSubmitted = true;
+    };
+
+    $scope.items = [];
+
+    function loadItems() {
+        $scope.items = shared.getCartItems();
+    }
+
+    loadItems();
+}
